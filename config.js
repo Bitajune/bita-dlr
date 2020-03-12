fdc74b19562321109c9f9d660360ae3b12f33603
Bitajune <beheshtibita@gmail.com>
2020-03-12 13:02:43 -0700

    remove footer

---
+++ config.js
@@ -0,0 +1,30 @@
+module.exports = {
+  siteTitle: 'Bita De La Rosa',
+  manifestName: 'Grayscale',
+  manifestShortName: 'Landing',
+  manifestStartUrl: '/',
+  manifestBackgroundColor: 'rgb(46, 61, 55)',
+  manifestThemeColor: '#663399',
+  manifestDisplay: 'standalone',
+  manifestIcon: 'src/assets/img/website-icon.png',
+  pathPrefix: `/gatsby-starter-grayscale/`, // This path is subpath of your hosting https://domain/portfolio
+  heading: 'Bita De La Rosa',
+  subHeading: 'Software Developer',
+
+  // social
+  socialLinks: [
+    {
+      icon: 'fa-github',
+      name: 'Github',
+      url: 'https://github.com/bitajune',
+    },
+    {
+      icon: 'fa-linkedin-in',
+      name: 'Linkedin',
+      url: 'https://www.linkedin.com/in/bitadelarosa/',
+    },
+  ],
+  email: 'bitadelarosa@gmail.com',
+  phone: '949-463-2482',
+  address: '13881 Tustin East Dr, Tustin, CA',
+};
